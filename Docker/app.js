d4d23afda9b2f807292733b3b1a7c070adb2b881
tiagopereyra <80712694+tiagopereyra@users.noreply.github.com>
2022-07-04 15:28:39 -0300

    Update app.js

--- Docker/app.js
+++ Docker/app.js
@@ -1,11 +1,20 @@
-// Express //
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
+
+const date = new Date()
+const [month, day, year] = [date.getMonth()+1, date.getDate(), date.getFullYear()];
+const response = (`${year}${month}${day}`)
+const hash = process.env.GIT_COMMIT
+
 app.get('/', (req, res) => {
-  res.send('Hello World! grupo 16 pai')
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+res.send("Hello world!!" + "V. " + hash + response)
+
 })
 
+app.listen(port, () => {
+
+console.log(`Example app listening on port ${port}`)
+
+})
