9eb0a92f0adc2b0d7dba862739db26ffb36c6d5c
emanuelruizbujno <emanuelruizbujno@gmail.com>
2022-07-04 15:58:31 -0300

    commit

--- Docker/__test__/prueba.test.js
+++ Docker/__test__/prueba.test.js
@@ -1,22 +1,15 @@
 const app = require("../../app");
 const request = require("supertest")(app);
-let token = "";
 
 beforeAll(async () => {
  
 })
 
-describe("Testing peliculas", () =>{
-
-    describe("Delete pelicula", () => {
-        test("It should be with code 200", async () => {
-        
-                const response = await request.get("/");
-                expect(response.statusCode).toBe(200)
-
-            });
-    })
-
+describe("test muy basico", () => {
+    test("It should be with code 200", async () => {
+            const response = await request.get("/");
+            expect(response.statusCode).toBe(200)
+        });
 })
 
 afterAll(async () => {
